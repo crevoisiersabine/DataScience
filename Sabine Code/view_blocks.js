0d3cfb70b8da2e5e0b37d0f2bff0f65ebc39b794
Sabine <crevoisiersabine@yahoo.fr>
2014-10-16 17:21:25 +0800

    updated functionality for storytelling and hovering

--- Sabine Code/view_blocks.js
+++ Sabine Code/view_blocks.js
@@ -1,6 +1,6 @@
 var margin = { top: 100, right: 10, bottom: 50, left: 50 };
-var width = 850 - margin.left - margin.right;
-var height = 500 - margin.bottom - margin.top;
+var width = 1000 - margin.left - margin.right;
+var height = 450 - margin.bottom - margin.top;
 var width_legend = 100;
 
 var format = d3.time.format("%d-%m-%Y");
@@ -53,8 +53,44 @@ var svg_legend = d3.select("#area1")
 dataLoader.load(fillCourses);
 
 function fillCourses() {
+  //Text to tell a story
+  var captions = ["<b>Starting with the 5 courses:<br></b>\
+                  <li>CS106B (c++)<br></li>\
+                  <li>Natural Language Processing<br></li>\
+                  <li>Databases<br></li>\
+                  <li>CS171 (visualisation)<br></li>\
+                  <li>Statistics (udacity)<br></li>\
+                  <b>As planned. For one month.</b>",
+                  "Followed by a two week holiday, visiting Thailand.<br>\
+                  <b>And a little work...</b>",
+                  "<b>CS171</b> was a <b>fantastic</b> course so we started looking for other Harvard courses and found:<br>\
+                   <br>\
+                  <b>CS109 (data science course). </b>",
+                  "<b>ALTER THE PLAN</b>\
+                  <ol><li>Put current courses on hold</li>\
+                  <li>Focus on NEW courses:</li></ol>\
+                  <li>CS109 (data science)</li>\
+                  <li>Stats110  (statistics pre-requisite for CS109)</li>",
+                  "Decide to focus on one course at a time.<br>\
+                   <br>\
+                  <b>Stats110 completed!</b>",
+                  "<b>CS109 completed!</b>",
+                  "<b>CS171 completed!</b>",
+                  "Decide to study <b>statistical inference</b> to supplement Stats110.<br>",
+                  "<b>Databases completed!</b>",
+                  "Onwards with c++ towards completion..."] 
   var courses = dataLoader.courses();
   color.domain(dataLoader.colorD());
+
+  //Create the labels
+  svg_plot.append("g").append("foreignObject")
+          .attr("id", "caption")
+          .attr("x", 570)
+          .attr("y", -70)
+          .attr('width', 350)
+          .attr('height', 500)
+          .append("xhtml:body")
+          .html('<div style="width: 400px; font-size: 30; color: white; display:none"></div>');
 
   //Create the x and y axes
   x.domain([dataLoader.minDate(), dataLoader.maxDate()]);
@@ -126,6 +162,7 @@ function fillCourses() {
     })
   }
 
+  //Populate the buckets depending on the date
   courses.forEach(function(day) {
 
     if (day[0].date < format.parse("01-07-2014")){
@@ -221,9 +258,28 @@ function fillCourses() {
   })
 
   // Transitions & update hours
-  for(bucket_number = 1; bucket_number < 11; bucket_number++){
-    transitions(bucket_number);
+  var rectsBucket = [];
+  rectsBucket.push(d3.select("#bucket1").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket2").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket3").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket4").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket5").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket6").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket7").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket8").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket9").selectAll("rect")[0].length);
+  rectsBucket.push(d3.select("#bucket10").selectAll("rect")[0].length);
+
+  var time_funcs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      timer = 0;
+  var delays = [3000, 2000, 4000, 4000, 4000, 2000, 2000, 3000, 3000]
+
+  function callFuncs() {
+    transitions(time_funcs[timer++], captions, rectsBucket);
+    if (timer < time_funcs.length) setTimeout(callFuncs, delays[timer-1]);
   }
+  //Launch the function after 0.5s
+  setTimeout(callFuncs, 0);
 
   //Legend -- SHOULD BE IN SAME BOOTSTRAP COLUMN AS GRAPH
   var legend = svg_legend.selectAll(".legend")
@@ -245,32 +301,45 @@ function fillCourses() {
     .style("text-anchor", "end")
     .text(function(d) { return d; });
 
+    //Hovering
+    d3.selectAll("rect")
+      .on("mouseover", function(){
+        //This checks the class of the caption to disallow hovering over the rects before the storytelling piece is over
+        if(d3.select("#caption").classed("10")){
+          var buck = this.parentNode.parentNode.id;
+          //Note buck_num = 0 actually corresponds to 10th bucket
+          var buck_num = buck[buck.length-1];
+          if(buck_num == 0){
+            $("#caption").html(captions[9]);
+          }
+          else{
+            $("#caption").html(captions[buck_num-1]);
+          }
+        }
+      })
 };
 
-function transitions(bucketSelection){
-
-    d3.selectAll("g").selectAll("#bucket" + bucket_number + " rect")
-      .transition()
-      .duration(100)
-      .delay(function(d, i){
-        return x(d.date) * 30 + i * 30;
+function transitions(bucket_number, captions, rectsBucket){
+  
+  // First transition the bars
+  d3.selectAll("g").selectAll("#bucket" + bucket_number + " rect")
+    .transition()
+    .duration(50)
+    .delay(function(d, i){
+      return x(d.date) * 10 + i * 10;
+    })
+      .style('opacity', 1)
+      .attr("class", "shown")
+      .each('end', function(d, i) {
+        var totalHours = parseInt(d3.select(".header .col-md-1 h1 small").text()) + 1;
+        d3.select(".header .col-md-1 h1 small").text(totalHours);
+        if(i == 0){
+          $("#caption").html(captions[bucket_number - 1]).attr("class", bucket_number).hide();
+        }
+        if(i == rectsBucket[bucket_number - 1] - 2){
+          // Then transition the relevant text
+          $("#caption").fadeIn();
+        }
       })
-        .style('opacity', 1)
-        .attr("class", "shown")
-        .each('end', function(d, i) { 
-          var totalHours = parseInt(d3.select(".header .col-md-1 h1 small").text()) + 1;
-          d3.select(".header .col-md-1 h1 small").text(totalHours);
-        })
-}
-
-//Doesn't work so far
-function setToRecede(bucket_number){
-  d3.selectAll("g").selectAll("#bucket_" + bucket_number)
-    .style("fill", "#C0C0C0")
-    .style('opacity', 0.1);
-}
-
-function getTotalHours(){
-  return (d3.selectAll(".shown").data())
 }
 
